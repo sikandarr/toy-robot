c6a394689043517ac18a465239ca44fa4aca5f04
Sikandar <sikandar.b@mail.com>
2018-03-24 11:03:29 +1030

    Added comments

--- src/main/java/com/reagroup/toyrobot/model/Surface.java
+++ src/main/java/com/reagroup/toyrobot/model/Surface.java
@@ -1,7 +1,21 @@
-package com.reagroup.toyrobot;
+package com.reagroup.toyrobot.model;
 
+import com.reagroup.toyrobot.Position;
+
+/**
+ * The Surface interface represents a plane on which 
+ * the objects of our universe exist; there's not much known
+ * about the Surface - it could, for example, be a square,
+ * a circle, a pentagon, or even the white house, it doesn't matter.
+ * What matters is that any implementation of this artifact be able to
+ * tell us whether a given position is within its boundary.
+ * 
+ * Note: implementations that are one dimensional should necessarily
+ * render all positions that have the other dimension greater than zero
+ * beyond its boundary.
+ *
+ */
 public interface Surface
 {
 	public boolean isPositionWithinBounds(Position location);
-
 }
