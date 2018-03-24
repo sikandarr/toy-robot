384c869b8aa4f268495dcc51c26cd63f387cf427
Sikandar <sikandar.b@mail.com>
2018-03-24 12:10:29 +1030

    Added test for Rotations

---
+++ src/test/java/com/reagroup/tests/TestDirection.java
@@ -0,0 +1,48 @@
+package com.reagroup.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.reagroup.toyrobot.model.Direction;
+
+public class TestDirection
+{
+	private static Direction facing;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+		facing = Direction.NORTH;
+	}
+
+	@Test
+	public void testAllRotations()
+	{
+		facing = facing.turnLeft();
+		assertEquals(Direction.EAST, facing);
+		
+		facing = facing.turnLeft();
+		assertEquals(Direction.SOUTH, facing);
+		
+		facing = facing.turnLeft();
+		assertEquals(Direction.WEST, facing);
+		
+		facing = facing.turnLeft();
+		assertEquals(Direction.NORTH, facing);
+		
+		facing = facing.turnRight();
+		assertEquals(Direction.WEST, facing);
+		
+		facing = facing.turnRight();
+		assertEquals(Direction.SOUTH, facing);
+		
+		facing = facing.turnRight();
+		assertEquals(Direction.EAST, facing);
+		
+		facing = facing.turnRight();
+		assertEquals(Direction.NORTH, facing);
+	}
+
+}
