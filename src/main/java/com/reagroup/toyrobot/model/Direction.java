2072b397b947afb4732f1d4d379759e6a1e91202
Sikandar <sikandar.b@mail.com>
2018-03-24 11:42:42 +1030

    Added Direction to Model

---
+++ src/main/java/com/reagroup/toyrobot/model/Direction.java
@@ -0,0 +1,36 @@
+package com.reagroup.toyrobot.model;
+
+/**
+ * An enumeration of Directions as predefined in the specs i.e.
+ * North, East, South, West.
+ * 
+ * Any changes to the definition of Direction, for example, 
+ * inclusion of Directions such as NorthEast or SouthWest will 
+ * invariably impact the code of this data type.
+ */
+public enum Direction
+{
+	NORTH, EAST, SOUTH, WEST;
+
+	private Direction fromValue(int value)
+	{
+		value = value % 4;
+		if (value == 3)
+			return WEST;
+		else if (value == 2)
+			return SOUTH;
+		else if (value == 1)
+			return EAST;
+		else return NORTH;
+	}
+
+	public Direction turnLeft()
+	{
+		return fromValue(this.ordinal() + 1);
+	}
+
+	public Direction turnRight()
+	{
+		return fromValue(this.ordinal() - 1);
+	}
+}
