ed3cd3bc8f190604dc994b498d07be7f3e56407b
Sikandar <sikandar.b@mail.com>
2018-03-24 15:38:05 +1030

    Added comments and changed methods to static

--- src/main/java/com/reagroup/toyrobot/controller/InputHandler.java
+++ src/main/java/com/reagroup/toyrobot/controller/InputHandler.java
@@ -1,24 +1,53 @@
-package com.reagroup.toyrobot;
+package com.reagroup.toyrobot.controller;
 
-import com.reagroup.toyrobot.commands.Command;
-import com.reagroup.toyrobot.commands.UnknownCommand;
-import com.reagroup.toyrobot.commands.PlaceCommand;
-import com.reagroup.toyrobot.commands.ReportCommand;
+import com.reagroup.toyrobot.controller.commands.*;
+import com.reagroup.toyrobot.model.Direction;
+import com.reagroup.toyrobot.model.Position;
+
+/**
+ * This class is responsible to translate user inputs and
+ * return the respective commands.
+ * <p>
+ * Note: This class is NOT responsible to inject properties 
+ * into Command objects that maybe required by these objects to
+ * perform their service with the exception of cases of inputs that
+ * contain arguments - for example, PLACE command.
+ *
+ */
 
 public class InputHandler
 {
-	public Command handleInput(String command)
+	/**
+	 * Returns a Command object that can then be executed
+	 * by a simulation engine. It is the responsibility of
+	 * the client to ensure that relevant dependencies are injected 
+	 * into the returned object before execution.
+	 * 
+	 * @param command String to be interpreted
+	 * @return the corresponding command or an UnknownCommand 
+	 * when the argument can-not be interpreted
+	 */
+	public static Command handleInput(String command)
 	{
 		if (command.startsWith("PLACE"))
 			return parsePlaceCommand(command);
 
 		if (command.equals("REPORT"))
-			return new ReportCommand(System.out);
+			return new ReportCommand();
+		
+		if (command.equals("LEFT"))
+			return new LeftCommand();
+		
+		if (command.equals("RIGHT"))
+			return new ReportCommand();
+		
+		if (command.equals("MOVE"))
+			return new MoveCommand();
 
 		return new UnknownCommand();
 	}
 
-	private Command parsePlaceCommand(String command)
+	private static Command parsePlaceCommand(String command)
 	{
 		String[] args = command.split(" ");
 		
