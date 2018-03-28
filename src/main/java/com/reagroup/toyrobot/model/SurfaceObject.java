3207180587cf1cc7598eaa0808f2f9d4faa1202c
Sikandar <sikandar.b@mail.com>
2018-03-28 14:50:22 +1030

    Added comments on performable actions list

--- src/main/java/com/reagroup/toyrobot/model/SurfaceObject.java
+++ src/main/java/com/reagroup/toyrobot/model/SurfaceObject.java
@@ -8,21 +8,34 @@ package com.reagroup.toyrobot.model;
 import java.util.List;
 
 import com.reagroup.toyrobot.simulation.actions.Action;
+import lombok.Getter;
 
-import lombok.Data;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@RequiredArgsConstructor
 public abstract class SurfaceObject
 {
+	@Getter
 	protected Position position;
-	
-	@NonNull
 	protected String name;
-	@NonNull
-	protected List<Action> actions;
+
+	/**
+	 * This is the list of actions that a surface object can perform;
+	 * this list as a property of this class only acts as a boolean
+	 * placeholder - i.e. the methods of the actions are never called
+	 * the presence action instances only serve the purpose of
+	 * validating an objects ability to perform a given action.
+	 * While, this choice may seem lumpy it makes our object dynamic
+	 * -- all actions that exist in the system can be added or
+	 * removed from outside the class or even at runtime.
+	 * 
+	 * Note: this list should ideally be unique and we can force this constraint
+	 * either by restricting its access to subclasses or by using sets but what
+	 * we have here serves our present needs.
+	 */
+	private List<Action> actions;
+
+	protected SurfaceObject(List<Action> actions)
+	{
+		this.actions = actions;
+	}
 
 	public void performAction(Action action)
 	{
@@ -31,19 +44,20 @@ public abstract class SurfaceObject
 				action.performAction(this);
 	}
 
-	public void addAction(Action action)
+	protected void addAction(Action action)
 	{
-		actions.add(action);
+		if (actions.indexOf(action) == -1)
+			actions.add(action);
 	}
 
-	public boolean removeAction(Action action)
+	protected void removeAction(Action action)
 	{
-		return actions.remove(action);
+		actions.remove(action);
 	}
 
 	@Override
 	public String toString()
 	{
-		return position.toString();
+		return name + "Position: " + position.toString();
 	}
 }
