92a5f94857bff3caee181f27640fef77507b5566
Sikandar <sikandar.b@mail.com>
2018-04-01 18:26:33 +0930

    Add comments to ValidationService

--- src/main/java/com/reagroup/toyrobot/simulation/validation/ValidationService.java
+++ src/main/java/com/reagroup/toyrobot/simulation/validation/ValidationService.java
@@ -4,7 +4,16 @@ import java.util.Observer;
 
 /**
  * This interface provides validation service to
- * all Observable objects that register its service
+ * all Observable objects that register its service.
+ * Its an Observer of the observer pattern. 
+ * 
+ * Whilst its possible to embed the logic of validating
+ * an action directly into Action classes - we've decoupled
+ * this responsibility from Action classes. This allows
+ * greater extensibility of both the action and validation services;
+ * for example, we can introduce new actions without repeating the
+ * code required to validate those actions or impose new restrictions
+ * with much change in the existing actions classes.
  *
  */
 
