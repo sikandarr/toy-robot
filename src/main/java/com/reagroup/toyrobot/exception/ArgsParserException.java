241db85576cc5e64526909b563a2dded8a1136e4
Sikandar <sikandar.b@mail.com>
2018-04-03 02:16:05 +0930

    Add ArgsParser, a Singleton

---
+++ src/main/java/com/reagroup/toyrobot/exception/ArgsParserException.java
@@ -0,0 +1,11 @@
+package com.reagroup.toyrobot.exception;
+
+public class ArgsParserException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+	public ArgsParserException(String message)
+	{
+		super(message);
+	}
+
+}
